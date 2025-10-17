0c662ab0d0b81a40f8887c8a433f891a9d8be5fc
Bondarenko Roman <bondarenkoromanupwork@gmail.com>
2025-10-17 20:19:26 +0300

    setup project context

---
+++ Assets/Scripts/Infrastructure/Installers/InfrastructureInstaller.cs
@@ -0,0 +1,11 @@
+using Zenject;
+
+namespace Infrastructure.Installers
+{
+    public class InfrastructureInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+        }
+    }
+}
