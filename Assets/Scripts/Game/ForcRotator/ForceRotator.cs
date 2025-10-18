1616d34ab401685143ddfc0bd94571f689c59d55
Bondarenko Roman <bondarenkoromanupwork@gmail.com>
2025-10-18 12:52:08 +0300

    add ForceRotator

---
+++ Assets/Scripts/Game/ForcRotator/ForceRotator.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceRotator : MonoBehaviour
+{
+    [SerializeField] private float _turnInPlaceAngularSpeedDeg = 360f;
+    [SerializeField] private float _turnAroundCooldown = 0.2f;
+
+    private bool _isTurningAround;
+    private float _turnTargetAngleDeg;
+    private float _turnCooldownUntil;
+
+    public event Action OnRotationFinished;
+
+    public void TurnAroundStart()
+    {
+        if (_isTurningAround)
+            return;
+
+        _isTurningAround = true;
+        float currentAngleDeg = Mathf.Atan2(transform.up.y, transform.up.x) * Mathf.Rad2Deg - 90f;
+        _turnTargetAngleDeg = Mathf.Repeat(currentAngleDeg + 180f, 360f);
+    }
+
+    public void TurnAroundProcess()
+    {
+        float currentAngleDeg = Mathf.Atan2(transform.up.y, transform.up.x) * Mathf.Rad2Deg - 90f;
+        float maxStep = _turnInPlaceAngularSpeedDeg * Time.deltaTime;
+        float newAngleDeg = Mathf.MoveTowardsAngle(currentAngleDeg, _turnTargetAngleDeg, maxStep);
+        transform.rotation = Quaternion.AngleAxis(newAngleDeg, Vector3.forward);
+
+        if (Mathf.DeltaAngle(newAngleDeg, _turnTargetAngleDeg) == 0f)
+        {
+            TurnAroundFinish();
+        }
+    }
+
+    private void TurnAroundFinish()
+    {
+        _isTurningAround = false;
+        _turnCooldownUntil = Time.time + _turnAroundCooldown;
+        OnRotationFinished.Invoke();
+    }
+}
